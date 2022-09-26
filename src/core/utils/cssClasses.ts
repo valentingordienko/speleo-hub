312e67664d02c17d204e06649114224446320733
valentin gordienko <valentingordienko.dev@yandex.ru>
2022-09-26 23:08:22 +0300

    Доработка утилиты cssClasses

--- src/core/utils/cssClasses.ts
+++ src/core/utils/cssClasses.ts
@@ -1,24 +1,29 @@
-type TArg = string | string[] | Record<string, boolean>
+type TPrimitive = undefined | null | string
+type TArg = TPrimitive | TPrimitive[] | Record<string, boolean>
 
 export function cssClasses(...arg: TArg[]): string {
     const values: Set<string> = new Set([])
-    arg.forEach((item)=> {
-        if(typeof item === 'string') {
-            values.add(item)
-        } else if(Array.isArray(item)) {
-            item.forEach((i)=> {
-                values.add(i)
-            })
-        } else if(typeof item === 'object') {
-            const data = Object.entries(item)
-            data.forEach(([key, value])=> {
-                if(value) {
-                    values.add(key)
-                }
-            })
+    arg.forEach((item) => {
+        if (item) {
+            if (typeof item === 'string') {
+                values.add(item)
+            } else if (Array.isArray(item)) {
+                item.forEach((i) => {
+                    if (i) {
+                        values.add(i)
+                    }
+                })
+            } else if (typeof item === 'object') {
+                const data = Object.entries(item)
+                data.forEach(([key, value]) => {
+                    if (value) {
+                        values.add(key)
+                    }
+                })
+            }
         }
     })
     let result: string = ''
-    values.forEach((item)=> result += ` ${item}`)
+    values.forEach((item) => result += ` ${item}`)
     return result.trim()
 }
