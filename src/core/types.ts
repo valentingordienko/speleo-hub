ba8356841d05842df9f937698bd4f92c57b77f43
valentin gordienko <valentingordienko.dev@yandex.ru>
2022-09-26 23:09:16 +0300

    Добавление типов базовых свойств

---
+++ src/core/types.ts
@@ -0,0 +1,12 @@
+export type TClassName = {
+    className?: string
+}
+export type TDisabled = {
+    disabled?: boolean
+}
+export type TReadonly = {
+    readonly?: boolean
+}
+export type TProcessing = {
+    processing?: boolean
+}
